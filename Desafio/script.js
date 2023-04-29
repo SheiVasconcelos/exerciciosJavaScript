8ee1ac1f1d142e9a562b53c735b17c666668c649
Sheila Vasconcelos <130600782+SheiVasconcelos@users.noreply.github.com>
2023-04-29 15:35:01 -0300

    Backup desafio - Decodificador

---
+++ Desafio/script.js
@@ -0,0 +1,44 @@
+/*
+var a = "ai"
+var e = "enter"
+var i = "imes"
+var o = "obes"
+var u = "ufat"
+*/
+
+function criptografar () {
+	var input = document.getElementById("palavra").value;
+	var lista = input.split("");
+
+	for (var x = 0; x < lista.length; x++) {
+		if (lista[x]=="a") lista[x]="ai"
+		else if (lista[x]=="e") lista[x]="enter"
+		else if (lista[x]=="i") lista[x]="imes"
+		else if (lista[x]=="o") lista[x]="obes"
+		else if (lista[x]=="u") lista[x]="ufat"
+	}
+	
+	var output = lista.join("");
+	document.getElementById("saida").innerHTML= output;
+}
+
+function descriptografar () {
+	var input = document.getElementById("palavra").value;
+	var output = input
+
+	output = output.replaceAll ("obes", "o");
+	output = output.replaceAll ("ufat", "u");
+	output = output.replaceAll ("ai", "a");
+	output = output.replaceAll ("imes", "i");
+	output = output.replaceAll ("enter","e");
+	
+	document.getElementById("saida").innerHTML= output;
+	
+}
+
+function copiarTexto () {
+	var texto = document.getElementById("saida");
+	//texto.select();
+	navigator.clipboard.writeText(texto.innerHTML);
+
+}
